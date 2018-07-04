9d335c566ea0a6fc604b8a6181a18c9ea8cf72da
anildofabiano <37662034+anildofabiano@users.noreply.github.com>
2018-07-04 11:47:56 -0300

    Integrar API

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--
+AJS SERVIÇOS E SOLUÇÕES
+Desenvolvimento de Sistemas Web Personalizados.
+Linguagens  de programação: PHP, HTML5, CSS3, JavaScript, JSON, JQuery e SQL.
+-->
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>SIDE PROJECT 1</title>
+        <link rel="stylesheet" href="ajs-folha-estilo.css" type="text/css" media="all"/>
+    </head>
+    <body>
+        <header class="ajs-header">
+            <section>
+                <span>Faltam 10 vídeos</span>
+            </section>
+        </header>
+        <main class="ajs-main">
+            <!--<video>
+                <source src="" type="" />
+            </video>-->
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            <section>
+                <img src="bck.jpg" alt="" />
+            </section>
+            
+        </main>
+    </body>
+</html>
